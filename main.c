2ff4c6328cb3ac679371acd825027eabc5a247ee
David Jaramillo <dj24@dsyslinuxp01.unm.edu>
2026-09-23 13:25:00 -0600

    Working on code

--- main.c
+++ main.c
@@ -10,11 +10,55 @@ Array *average_adjacent(Array *a);
 int main(int argc, char *argv[])
 {
     //main code 
+if (argc != 2)
+    {
+//Prompt the user to enter an array size 
+        printf("Please enter the array size.\n");
+        return 1;
+    }
+
+    int size = atoi(argv[1]);
+
+    Array *array = malloc(sizeof(Array));
+    array->size = size;
+    array->data = malloc(size * sizeof(double));
+
+    for (int i = 0; i < size; i++)
+    {
+        array->data[i] = i + 1;
+    }
+
+    printf("Original array:\n");
+    output_array(array);
+
+    shift_array(array);
+
+    printf("Shifted array:\n");
+    output_array(array);
+
+    Array *average = average_adjacent(array);
+
+    printf("Average array:\n");
+    output_array(average);
+
+    free(array->data);
+    free(array);
+
+    free(average->data);
+    free(average);
+
+    return 0;
 }
 
 void output_array(Array *a)
 {
     // code
+ for (int i = 0; i < array->size; i++)
+    {
+        printf("%.1f ", array->data[i]);
+    }
+
+    printf("\n");
 }
 
 void shift_array(Array *a)
